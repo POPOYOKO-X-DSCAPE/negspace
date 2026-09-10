27d73a89792616b9b57110033550ca7fc4b01c7c
LouiFi (AI assisted) <louis@popoyoko.fr>
2026-09-10 14:41:45 +0000

    📦 sources b6d0287

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '../../../styled-system/styles.css';
+import '../styled-system/styles.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');
